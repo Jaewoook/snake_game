673223e035c865a05e3545c6987131e05ffabc37
Jaewook Ahn <ajw4586@gmail.com>
2021-06-05 16:06:49 +0900

    Implement snake

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,19 +1,34 @@
 #include "snake.h"
+#include "log.h"
 
-// void Snake::change_direction(SnakeDirection direction) {
-//     this->direction = direction;
-// }
-
-void Snake::increase() {
-    //  TODO
+Snake::Snake(): direction(RIGHT), snake_pos(std::deque<POSITION>()) {
+    log("Snake", "Init");
+    init_snake_pos();
 }
 
-void Snake::decrease() {
-    //  TODO
+void Snake::increase() {
+    log("Snake", "Increase snake");
+    DIRECTION tail_direction = get_tail_direction();
+    auto old_tail_pos = snake_pos[get_snake_size() - 1];
+    POSITION tail_pos { old_tail_pos.x, old_tail_pos.y };
+    switch (tail_direction) {
+    case UP:
+        tail_pos.y -= 1;
+        break;
+    case DOWN:
+        tail_pos.y += 1;
+        break;
+    case RIGHT:
+        tail_pos.x += 1;
+        break;
+    case LEFT:
+        tail_pos.x -= 1;
+        break;
+    }
+    snake_pos.push_back(tail_pos);
 }
 
 void Snake::reset() {
-    size = 2;
-    head_pos = POSITION { 10, 10 };
     direction = RIGHT;
+    init_snake_pos();
 }
--- src/snake.h
+++ src/snake.h
@@ -1,29 +1,37 @@
-#include <queue>
+#include <deque>
 #include "common.h"
 
 #ifndef __SNAKE__
 #define __SNAKE__
 
-#define SYM_SNAKE_HEAD L"🟨"
-
 class Snake {
     private:
-        std::queue<POSITION> snake_pos;
-        POSITION head_pos;
+        std::deque<POSITION> snake_pos;
         DIRECTION direction;
-        int size;
+        DIRECTION get_tail_direction() {
+            int size = snake_pos.size();
+            auto last_first = snake_pos[size - 1];
+            auto last_second = snake_pos[size - 2];
+
+            int diff_x = last_first.x - last_second.x;
+            int diff_y = last_first.y - last_second.y;
+            if (diff_x == 0) {
+                return diff_y == 1 ? DOWN : UP;
+            } else {
+                return diff_x == 1 ? RIGHT : LEFT;
+            }
+        }
+        void init_snake_pos() {
+            snake_pos.clear();
+            snake_pos.push_back(POSITION { 10, 10 });
+            // snake_pos.push_back(POSITION { 9, 10 });
+        }
 
     public:
-        Snake():
-            head_pos { 10, 10 },
-            direction(RIGHT),
-            size(2) {}
+        Snake();
         void increase();
-        void decrease();
-        POSITION get_head_pos() { return head_pos; }
-        void update_head_pos(int y, int x) {
-            head_pos = POSITION { x, y };
-        }
+        std::deque<POSITION> get_snake_pos() { return snake_pos; }
+        int get_snake_size() { return snake_pos.size(); }
         DIRECTION get_direction() { return direction; }
         void change_direction(DIRECTION direction) { this->direction = direction; }
         void reset();
